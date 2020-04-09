2e62d834876cba57f93e70c1e23b186220cdf03e
ronyshchenko <roma.onyshchenko@gmail.com>
2020-04-09 10:16:24 +0300

    added product-slider product

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,12 @@ $(function(){
     readOnly: true
   });
 
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
   var mixer = mixitup('.products__inner-box');
 });
